f8199ba6b02b0dd52af602a8d81ab1665efd6cdf
Fabio Mucciante <fabio.mucciante@gmail.com>
2023-08-29 14:54:16 +0200

    Added the destination folder creation when it doesn't exist.

--- common/install.sh
+++ common/install.sh
@@ -19,13 +19,18 @@ else
   dest_folder=`kde4-config --path services | cut -f 1 -d ':'`ServiceMenus
 fi
 
+echo "Source path: $src_folder" | tee -a $log_file
+if [ -d "$dest_folder" ]; then
+  echo "Destination path: $dest_folder" | tee -a $log_file
+else
+  mkdir -p "$dest_folder"
+  echo "Destination path created: $dest_folder" | tee -a $log_file
+fi
+
 echo "Parameter: $action" | tee -a $log_file
 case "$action" in
 '--install'|'--local'|'--local-install')
   echo "Installing: $desktop_filename" | tee -a $log_file
-  echo "Source path: $src_folder" | tee -a $log_file
-  echo "Destination path: $dest_folder" | tee -a $log_file
-
   if [ -f "$dest_folder/$desktop_filename" ]; then
     echo 'An old version has been detect, a backup will be performed!' | tee -a $log_file
     cp -b --suffix '.old' "$src_folder/$desktop_filename" "$dest_folder/"
@@ -37,6 +42,7 @@ case "$action" in
     echo 'Service Menu installed!' | tee -a $log_file
   else
     echo "Something went wrong installing $desktop_filename" | tee -a $log_file
+    exit 1
   fi
 ;;
 '--uninstall'|'--remove'|'--delete'|'--deinstall')
