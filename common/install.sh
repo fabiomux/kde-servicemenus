57f4ff7b27b47b0a47b8ab82eb7bda94e6a58278
Fabio Mucciante <fabio.mucciante@gmail.com>
2024-05-10 22:33:23 +0200

    Fixed the Plasma 6 detection for the Arch distro.

--- common/install.sh
+++ common/install.sh
@@ -2,8 +2,8 @@
 #
 # Authors: Fabio Mucciante
 # Created: 2021/08/31
-# Updated: 2024/03/04
-# Version: 1.1.0
+# Updated: 2024/02/10
+# Version: 1.2.0
 # License: GPL-v3
 #
 # References:
@@ -15,6 +15,7 @@ dest_folder=''
 desktop_filename='__project_name__.desktop'
 action=$1
 src_folder=$(dirname "$0")
+kde_version=''
 
 echo '[=] Starting procedure from install.sh' | tee $log_file
 if [ $# -eq 0 ]; then
@@ -23,22 +24,38 @@ if [ $# -eq 0 ]; then
 fi
 echo "[*] N. Parameters: $#" | tee -a $log_file
 
-if [ -n "$(command -v kf5-config)" ]; then
-  dest_folder=$(kf5-config --path services | cut -f 1 -d ':')ServiceMenus
+if [ -n "$(command -v plasmashell)" ]; then
+  kde_version="Plasma $(plasmashell -v | cut -d ' ' -f 2 | cut -d '.' -f 1)"
+elif [ -n "$(command -v kf5-config)" ]; then
+  kde_version='Plasma 5'
 elif [ -n "$(command -v kde4-config)" ]; then
-  dest_folder=$(kde4-config --path services | cut -f 1 -d ':')ServiceMenus
+  kde_version='KDE 4'
 elif [ -n "$(command -v qtpaths6)" ]; then
-  dest_folder=$(qtpaths6 --locate-dirs GenericDataLocation kio/servicemenus | cut -f 1 -d ':')
-  writable_path=$(qtpaths6 --writable-path GenericDataLocation)
-  dest_folder=$(echo "$dest_folder" | sed "s@^/usr/share@$writable_path@g")
-elif [ -n "$(command -v qtpaths)" ]; then
-  dest_folder=$(qtpaths --locate-dirs GenericDataLocation kio/servicemenus | cut -f 1 -d ':')
-  writable_path=$(qtpaths --writable-path GenericDataLocation)
-  dest_folder=$(echo "$dest_folder" | sed "s@^/usr/share@$writable_path@g")
+  kde_version='Plasma 6'
 else
   echo '[X] Unable to find the Service Menus path!'
   exit 1
 fi
+
+case $kde_version in
+'KDE 4')
+  dest_folder=$(kde4-config --path services | cut -f 1 -d ':')ServiceMenus
+;;
+'Plasma 5')
+  dest_folder=$(kf5-config --path services | cut -f 1 -d ':')ServiceMenus
+;;
+'Plasma 6')
+  dest_folder=$(qtpaths --locate-dirs GenericDataLocation kio/servicemenus | cut -f 1 -d ':')
+  writable_path=$(qtpaths --writable-path GenericDataLocation)
+  dest_folder=$(echo "$dest_folder" | sed "s@^/usr/share@$writable_path@g")
+;;
+*)
+  echo '[X] Unknown Desktop Environment'
+  exit 2
+;;
+esac
+
+echo "[*] Desktop Environment: $kde_version"
 
 echo "[*] Source path: $src_folder" | tee -a $log_file
 if [ -d "$dest_folder" ]; then
